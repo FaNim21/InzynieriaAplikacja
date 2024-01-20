0f5addc1bf0070cfd5dc7952b4c1ecacb4a109a1
FaNim21 <filipekbwo@wp.pl>
2024-01-20 18:10:06 +0100

    Dodaj pliki projektów.

---
+++ InzynieriaAplikacja/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace InzynieriaAplikacja
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ InzynieriaAplikacja/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace InzynieriaAplikacja
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
