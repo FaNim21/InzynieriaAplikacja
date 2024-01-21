f414a1ded4ecc2117082eff5f0b995915cbb9e88
FaNim21 <filipekbwo@wp.pl>
2024-01-21 01:24:59 +0100

    .........

--- InzynieriaAplikacja/Models/Training.cs
+++ InzynieriaAplikacja/Models/Training.cs
@@ -1,4 +1,5 @@
-﻿using Realms;
+﻿using MongoDB.Bson;
+using Realms;
 
 namespace InzynieriaAplikacja.Models;
 
@@ -6,5 +7,17 @@ public class Training : RealmObject
 {
     [PrimaryKey]
     [MapTo("_id")]
-    public int Id { get; set; }
+    public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
+
+    [MapTo("_name")]
+    public string Name { get; set; }
+
+    [MapTo("_time")]
+    public string Time { get; set; }
+
+    [MapTo("_spalone_kalorie")]
+    public float SpaloneKalorie { get; set; }
+
+    [MapTo("_czwiecznia")]
+    public string Cwiczenia { get; set; }
 }
--- InzynieriaAplikacja/ViewModels/MainViewModel.cs
+++ InzynieriaAplikacja/ViewModels/MainViewModel.cs
@@ -1,14 +1,31 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using InzynieriaAplikacja.Models;
+using MongoDB.Bson;
 using Realms;
 using Realms.Sync;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace InzynieriaAplikacja.ViewModels;
 
 public partial class MainViewModel : BaseViewModel
 {
     public Realm Realm { get; set; }
-    public FlexibleSyncConfiguration Sync { get; set; }
+    public FlexibleSyncConfiguration? Sync { get; set; }
+
+    [ObservableProperty]
+    private ObservableCollection<Training> trainings = new();
+
+
+    public MainViewModel()
+    {
+        /*var config = new RealmConfiguration(); // Default configuration
+        Realm.DeleteRealm(config); // Delete the Realm file
+        Realm = Realm.GetInstance(config); // Recreate the Realm*/
+
+        Realm = Realm.GetInstance();
+    }
 
     [RelayCommand]
     public async Task SignOut()
@@ -31,10 +48,77 @@ public partial class MainViewModel : BaseViewModel
     [RelayCommand]
     public async Task GetTraining()
     {
-        Sync = new FlexibleSyncConfiguration(App.RealmApp.CurrentUser!);
+        IsBusy = true;
+        /*Sync = new FlexibleSyncConfiguration(App.RealmApp.CurrentUser!)
+        {
+            PopulateInitialSubscriptions = (realm) =>
+            {
+                var myItems = realm.All<Training>();
+                realm.Subscriptions.Add(myItems);
+            }
+        };
+        Trace.WriteLine(Sync.PopulateInitialSubscriptions.ToString());
         Realm = await Realm.GetInstanceAsync(Sync);
-        //var users = Realm.All<User>();
+        Trace.WriteLine("asdasd");*/
 
-        await Application.Current.MainPage.DisplayAlert(Realm.All<Training>().ToString(), "", "OK");
+        trainings.Clear();
+        var _trainings = Realm.All<Training>();
+        foreach (Training training in _trainings)
+        {
+            trainings.Add(training); 
+            //Trace.WriteLine($"{training.Name}");
+        }
+
+        Trace.WriteLine(trainings.Count);
+
+        //await Application.Current.MainPage.DisplayAlert(Realm.All<Training>().ToString(), "", "OK");
+        IsBusy = false;
+    }
+
+    [RelayCommand]
+    public async Task AddTraining()
+    {
+        IsBusy = true;
+        try
+        {
+            Realm.Write(() =>
+            {
+                var todo = new Training
+                {
+                    Name = "asdasd123123213",
+                    Time = DateTime.UtcNow.ToString(),
+                    SpaloneKalorie = 1000,
+                    Cwiczenia = "eloo"
+                };
+                Realm.Add(todo);
+            });
+        }
+        catch (Exception ex)
+        {
+            await Application.Current.MainPage.DisplayPromptAsync("Error", ex.Message);
+        }
+        IsBusy = false;
+    }
+
+    [RelayCommand]
+    async Task DeleteTraining()
+    {
+        IsBusy = true;
+        try
+        {
+            Training lastTraining = trainings.LastOrDefault();
+
+            Realm.Write(() =>
+            {
+                Realm.Remove(lastTraining);
+            });
+
+            trainings.Remove(lastTraining);
+        }
+        catch (Exception ex)
+        {
+            await Application.Current.MainPage.DisplayPromptAsync("Error", ex.Message);
+        }
+        IsBusy = false;
     }
 }
